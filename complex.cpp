0c30ac18bf111ea7ce34a6ca4ffff7a4bf2e479c
matveiganz <matveiganz@gmail.com>
2021-09-28 18:01:29 +0300

    first commit

---
+++ complex.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <complex>
+#include "complex.hpp"
+
+#define TEST1 check(res.re,real(comp_res), res.im, imag(comp_res))
+#define INIT aa = a;\
+comp_aa = comp_a;
+#define TEST2 check(aa.re,real(comp_aa), aa.im, imag(comp_aa))
+
+void check(double r1, double r2, double i1, double i2) {
+	if ((abs(r1 - r2) < 0.00001)&&(abs(i1 - i2) < 0.00001)) 
+		printf("Accepted\n");
+	else
+		printf("Failed\n");
+}
+
+int main() {
+	std::complex<double> comp_a = {3, 4}, comp_b = {5, -7}, comp_aa, comp_bb, comp_res;
+	complex_n a = {3, 4}, b = {5, -7}, aa, bb, res;
+	
+	res = -a;
+	comp_res = -comp_a;
+	TEST1;
+	res = a + b;
+	comp_res = comp_a + comp_b;
+	TEST1;
+	res = a - b;
+	comp_res = comp_a - comp_b;
+	TEST1;
+	res = a * b;
+	comp_res = comp_a * comp_b;	
+	TEST1;
+	res = a / b;
+	comp_res = comp_a / comp_b;
+	TEST1;
+	INIT;
+	aa += b;
+	comp_aa += comp_b;
+	TEST2;
+	INIT;
+	aa -= b;
+	comp_aa -= comp_b;
+	TEST2;
+	INIT;
+	aa *= b;
+	comp_aa *= comp_b;
+	TEST2;
+	INIT;
+	aa /= b;
+	comp_aa /= comp_b;
+	TEST2;
+	res = 3 * a;
+	comp_res = 3.0 * comp_a;
+	TEST1;
+	res = a * 3;
+	comp_res = comp_a * 3.0;
+	TEST1;
+
+	return 0;
+}
---
+++ complex.hpp
@@ -0,0 +1,47 @@
+#ifndef COMPLEX_HPP
+#define COMPLEX_HPP
+
+#include <cmath>
+
+struct complex_n {
+	
+	complex_n();
+	complex_n(double);
+	complex_n(double, double);
+	complex_n(const complex_n &);
+
+	complex_n& operator=(const complex_n &);
+	bool operator==(const complex_n &) const;
+	bool operator!=(const complex_n &) const;
+
+	complex_n& operator+=(const complex_n &);
+	complex_n& operator-=(const complex_n &);
+	complex_n& operator*=(const complex_n &);
+	complex_n& operator/=(const complex_n &);
+	
+	complex_n operator-() const;
+	complex_n operator*(double) const;
+	complex_n operator+(const complex_n &) const;
+	complex_n operator-(const complex_n &) const;
+	complex_n operator*(const complex_n &) const;
+	complex_n operator/(const complex_n &) const;
+	
+	complex_n operator^(const double) const;
+
+	complex_n conjugate() const;
+
+	double real() const;
+	double imag() const;
+	double arg() const;
+	double abs() const;
+	double sq_abs() const;
+	
+	~complex_n();
+
+	double re;
+	double im;
+};
+
+complex_n operator*(double, const complex_n);
+
+#endif
